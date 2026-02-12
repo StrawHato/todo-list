882d7533770377605ecc60f2253f1730fb762cc7
Maksym Pakula <gargantadoto@gmail.com>
2026-02-12 19:47:24 +0200

    Add required models for project

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ("created_at",)
+
+    def __str__(self):
+        return self.content
