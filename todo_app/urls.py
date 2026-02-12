f0914a0d46ca65a3044667594f05350da820acf6
Maksym Pakula <gargantadoto@gmail.com>
2026-02-12 23:35:00 +0200

    Add oportunity to undo/complete tasks

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from todo_app.views import (
     TodoListView,
-    TagListView
+    TagListView,
+    toggle_complete_task
 )
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="todo-list"),
     path("tags/", TagListView.as_view(), name="tag-list"),
+    path("<int:pk>/toggle-complete/", toggle_complete_task, name="toggle-complete"),
 ]
 
 app_name = "todo_app"
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import generic
 
 from todo_app.models import Task, Tag
@@ -10,3 +11,13 @@ class TodoListView(generic.ListView):
 
 class TagListView(generic.ListView):
     model = Tag
+
+
+def toggle_complete_task(request, pk):
+    task = Task.objects.get(id=pk)
+    if task.status:
+        task.status = False
+    else:
+        task.status = True
+    task.save()
+    return redirect("todo_app:todo-list")
